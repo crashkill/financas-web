c063ee4787d771502432e0ab92ef64c36353a0a0
Fabricio Cardoso <fabriciocardoso@MacBook-Pro-de-Fabricio.local>
2020-02-18 23:54:33 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import logo from "./assets/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <img src={logo} alt="AirCnC " />
+      <div className="content">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          O sistema de <strong>Gerenciamento Financeiro</strong> dos projetos
+          internos da <strong>GlobalHitss</strong>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form>
+          <label htmlFor="email">Digite o e-mail</label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Digite o seu e-mail corporativo"
+          />
+          <button className="btn" type="submit">
+            Entrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
